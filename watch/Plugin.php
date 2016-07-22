6a149ce57d872d85576684091d82405867e7b585
Joppe Aarts <joppe@zicht.nl>
2016-07-22 16:10:15 +0200

    initial commit

---
+++ watch/Plugin.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @author Joppe Aarts <joppe@zicht.nl>
+ * @copyright Zicht Online <http://zicht.nl>
+ */
+
+namespace Zicht\Tool\Plugin\Systemjs;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Zicht\Tool\Plugin as BasePlugin;
+
+/**
+ * Class Plugin
+ *
+ * @package Zicht\Tool\Plugin\Watch
+ */
+class Plugin extends BasePlugin
+{
+    /**
+     * @param ArrayNodeDefinition $rootNode
+     * @return void
+     */
+    public function appendConfiguration(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode
+            ->children()
+                ->arrayNode('watch')
+                    ->children()
+                        ->scalarNode('config')
+                            ->defaultValue('zwatch.json')
+                        ->end()
+                        ->scalarNode('zwatch')
+                            ->defaultValue('/usr/local/bin/zwatch')
+                            ->beforeNormalization()
+                                ->always(function($v) {
+                                    if (is_file($v)) {
+                                        return realpath($v);
+                                    }
+                                })
+                                ->end()
+                            ->validate()
+                                ->ifTrue(function($f) {
+                                    return !is_file($f);
+                                })
+                                ->thenInvalid('File does not exist. Do you need to install zwatch?')
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+}
